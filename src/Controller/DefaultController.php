3160a69851f3a9064d522a694350531d822bc6d4
lilou <michellisa.75@gmail.com>
2020-09-09 14:50:39 +0200

    update homepage

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class DefaultController extends AbstractController
@@ -10,9 +11,11 @@ class DefaultController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(Article $articles)
     {
-        return $this->render('default/index.html.twig');
+        return $this->render('default/index.html.twig',[
+            'articles' =>$articles
+        ]);
     }
 
 
